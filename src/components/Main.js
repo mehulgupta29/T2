2d44ab78924c75f756be045e357cb44cc7951ac8
Mehul Gupta <MehulGupta@Mehuls-MBP.fios-router.home>
2017-06-24 18:20:28 -0400

    added google signup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,61 @@
-require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import LandingComponent from "./Landing/LandingComponent";
+import LoginComponent from './Startup/LoginComponent';
+import * as firebase from 'firebase';
 
-let yeomanImage = require('../images/yeoman.png');
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDibGfWwpCxqUlgAfj6UYGMkudOH9UYcI0",
+  authDomain: "treatster2-31d5f.firebaseapp.com",
+  databaseURL: "https://treatster2-31d5f.firebaseio.com",
+  projectId: "treatster2-31d5f",
+  storageBucket: "",
+  messagingSenderId: "879934100019"
+};
 
 class AppComponent extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isUserLoggedIn: false,
+      googleUser: null
+    };
+  }
+
+  componentWillMount() {
+    firebase.initializeApp(config);
+
+    /**
+     * Checking if the user is already logged in.
+     * If true, route the user to LandingComponent
+     * Else, user is asked to 'login' or 'sign up'
+     * */
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in.
+        this.setState({isUserLoggedIn: true, googleUser: user});
+      }
+    });
+  }
+
   render() {
+    const {isUserLoggedIn, googleUser} = this.state;
+
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div style={appStyles}>
+        {isUserLoggedIn ? <LandingComponent googleUser={googleUser}/> : <LoginComponent firebase={firebase}/>}
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
-
 export default AppComponent;
+
+const appStyles = {
+  textAlign: 'center',
+  height: '100vh',
+  width: '100vw'
+};
