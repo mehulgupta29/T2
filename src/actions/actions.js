f6857eb55f8de3c6f9f72a399186301abc5d35d7
Mehul Gupta <MehulGupta@Mehuls-MBP.fios-router.home>
2017-06-24 23:20:06 -0400

    created persistent and realtime firebase database

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,13 +1,21 @@
 export const actionTypes = {
   SET_NEW_USER_DATA: "SET_NEW_USER_DATA",
-  LOGOUT: "LOGOUT"
+  LOGOUT: "LOGOUT",
+  PUT_USERS: "PUT_USERS"
 };
 
 const actions = {
-  setNewUserData(userData) {
+  setNewUserData(userData, firebase) {
     return {
       type: actionTypes.SET_NEW_USER_DATA,
-      data: userData
+      data: userData,
+      firebase: firebase
+    }
+  },
+  putUsers(users) {
+    return {
+      type: actionTypes.PUT_USERS,
+      data: users
     }
   },
   logout() {
--- src/stores/reducers.js
+++ src/stores/reducers.js
@@ -6,11 +6,28 @@ function putUserIfNotExists(users, newUser) {
   return isUserPresent ? users : _.concat(users, newUser);
 }
 
-
 export default function reducer(state, action){
   switch(action.type) {
     case actionTypes.SET_NEW_USER_DATA:
-      return Object.assign({}, state, {currentUser: action.data, users: putUserIfNotExists(state.users, action.data)});
+
+      // we are storing users list in firebase
+      const database = action.firebase.database();
+
+      let usersList = [];
+      database.ref('/').once('value', snapshot => {
+        usersList = _.has(snapshot.val(), 'users') ?
+          putUserIfNotExists(snapshot.val().users, action.data) : putUserIfNotExists([], action.data);
+
+        database.ref('/').set({users: usersList});
+      });
+
+      // storing currentUser in redux store
+      return Object.assign({}, state, {currentUser: action.data});
+
+    // case actionTypes.PUT_USERS:
+    //   console.log("hi");
+    //   // return Object.assign({}, state, {users: action.data});
+    //       return state;
 
     case actionTypes.LOGOUT:
       return Object.assign({}, state, {currentUser: null});
@@ -19,4 +36,3 @@ export default function reducer(state, action){
       return state;
   }
 }
-
